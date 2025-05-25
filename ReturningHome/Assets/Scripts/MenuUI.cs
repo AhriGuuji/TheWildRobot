4ed1f72f3ea634892b6f6bd9aa9b7d9c1db72a1e
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-05-25 01:16:30 +0100

    Reapply "Start Menu code is finished"
    This reverts commit 58404629d846388d67f0cbfd07190ee67ca19675.

---
+++ ReturningHome/Assets/Scripts/MenuUI.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour
+{
+    [SerializeField] private string _firstLevel;
+    [SerializeField] private string _thisSceneName;
+    [SerializeField] private Canvas _settingsMenu;
+    [SerializeField] private Canvas _startMenu;
+    [SerializeField] private Slider _soundSlider;
+    [SerializeField] private AudioSource _audioSource;
+    private float _volume = 1f;
+
+    void Start()
+    {
+        _settingsMenu.enabled = false;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(_firstLevel);
+    }
+
+    public void OpenSettings()
+    {
+        _settingsMenu.enabled = true;
+        if (_startMenu.enabled)
+            _startMenu.enabled = false;
+    }
+
+    public void CloseSettings()
+    {
+        _settingsMenu.enabled = false;
+        if (SceneManager.GetActiveScene().name == _thisSceneName)
+            _startMenu.enabled = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void UpdateVolume(float volume)
+    {
+        _volume = volume;
+        _audioSource.volume = _volume;
+        _audioSource.mute = (_volume <= 0);
+    }
+}
